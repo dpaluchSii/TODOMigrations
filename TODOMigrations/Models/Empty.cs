91b5a4ec49e2cb92321645488b819543cb3180fe
Dominik Paluch <dpaluch@SIIPOLSKA.PL>
2016-07-07 10:48:55 +0200

    add app ToDo

---
+++ TODOMigrations/Models/Empty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TODOMigrations.Models
+{
+    public class Empty
+    {
+    }
+}
---
+++ TODOMigrations/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TODOMigrations.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+
+        public string Event { get; set; }
+
+        public string Where { get; set; }
+
+        public DateTime When { get; set; }
+        //
+    }
+}
