550018dfadcf3b7c47a44b084834e7c560ffaad8
dpaluch <dpaluch@52KLJ72>
2016-07-11 12:58:59 +0200

    add require to field Event

--- TODOMigrations/Models/Task.cs
+++ TODOMigrations/Models/Task.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace TODOMigrations.Models
 {
+
     public class Task
     {
         public int TaskID { get; set; }
 
+        
+        [Required(ErrorMessage = "Event is required.")]
         public string Event { get; set; }
+
+        //public string Eventt { get; set; }
 
         public string Where { get; set; }
 
         public DateTime When { get; set; }
-        //
+        
     }
 }
